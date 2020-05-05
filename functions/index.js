db2917308d2d2c4b714326f35ff26685aea15776
Tsuyoshi Maeda <oasis5tsuyoshi@gmail.com>
2020-05-05 11:12:50 +0900

    Removed unused code.

--- functions/index.js
+++ functions/index.js
@@ -8,11 +8,4 @@ const app = express();
 app.use(cors());
 app.use('/', quizRouter);
 
-// // Create and Deploy Your First Cloud Functions
-// // https://firebase.google.com/docs/functions/write-firebase-functions
-//
-// exports.helloWorld = functions.https.onRequest((request, response) => {
-//   response.send("Hello from Firebase!!!!");
-// });
-
 exports.api = functions.region('asia-northeast1').https.onRequest(app);
