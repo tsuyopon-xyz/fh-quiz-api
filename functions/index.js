effde0bc6160c5c2a559021030f17df3cd41b471
Tsuyoshi Maeda <oasis5tsuyoshi@gmail.com>
2020-05-05 10:48:08 +0900

    Added cors.

--- functions/index.js
+++ functions/index.js
@@ -1,9 +1,11 @@
 const functions = require('firebase-functions');
 const express = require('express');
+const cors = require('cors')
 const quizRouter = require('./routes/quiz/router');
 
 const app = express();
 
+app.use(cors());
 app.use('/', quizRouter);
 
 // // Create and Deploy Your First Cloud Functions
